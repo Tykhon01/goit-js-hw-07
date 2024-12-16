2a0bc5cd5546ef84eeac12dd45dbcf71b1ff0004
Tykhon Shevchuk <tykhon000shevchuk@gmail.com>
2024-12-16 22:11:33 +0200

    all

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(`.login-form`);
 
+form.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+    const emailInput = form.elements.email;
+    const paswornInput = form.elements.password;
+
+    const email = emailInput.value.trim();
+    const password = paswornInput.value.trim();
+    
+    if (!email || !password) {
+        alert('All form fields must be filled in');
+        return;
+    }
+    const formData = {
+        email: email,
+        password: password,
+    }
+    console.log(formData);
+
+    form.reset();
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector(`.input`);
+const createBtn = document.querySelector(`[data-create]`);
+const destroyBtn = document.querySelector(`[data-destroy]`);
+const boxesContainer = document.querySelector(`#boxes`);
+
+function createBoxes(amount) {
+
+  destroyBoxes();
+
+  let size = 30;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement(`div`);
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.marginBottom = `5px`;
+
+    boxes.push(box);
+    size += 10;
+  }
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = ``;
+}
+
+createBtn.addEventListener(`click`, () => {
+  
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = ``;
+  } else {
+    alert(`Введіть число від 1 до 100!`)
+  }
+});
+
+destroyBtn.addEventListener(`click`, destroyBoxes);
